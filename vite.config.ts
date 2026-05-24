f5808bce734b0688c62d4d4d4ab950ecbe62a887
Damian Sikorski <sikorski65@gmail.com>
2026-05-24 18:11:39 +0200

    more maintanable

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/structure_ordi/', // Remplacer par le nom de ton dépôt GitHub
-  build: {
-    outDir: 'docs' // Force la compilation dans le dossier "docs"
-  },
   plugins: [react()],
 })
